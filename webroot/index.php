939d356218a7d3c540ec6d65c02839fe3bcbe407
Harvey Fletcher <harvey.fletcher1@ntlworld.com>
2018-10-25 07:47:00 +0100

    Template webpage with css file

--- webroot/index.php
+++ webroot/index.php
@@ -4,8 +4,15 @@
 <html>
     <head>
         <title>Harvey Fletcher DJ | HOME</title>
+		<link rel="stylesheet" href="main.css" type="text/css" />
     </head>
     <body>
-        Server configured successfully.
+		<div class="obscurity">&nbsp;</div>
+		<div class="links">
+			&nbsp;
+		</div>
+		<div class="content">
+			&nbsp;
+		</div>
     </body>
 </html>
